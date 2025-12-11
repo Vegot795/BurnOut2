dc612b8f85e3283b0ee5bb75fbfab948e1da799d
Vegot795 <167176338+Vegot795@users.noreply.github.com>
2025-12-11 01:46:55 +0100

    Działa

--- Burn_Out/Burn_Out/Program.cs
+++ Burn_Out/Burn_Out/Program.cs
@@ -25,9 +25,10 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=burnout.db";
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlite(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
