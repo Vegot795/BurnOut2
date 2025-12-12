b6bb8e51de19a65471f1e5b4453c6cb1dbadf4c5
Davies <167176338+Vegot795@users.noreply.github.com>
2025-12-12 02:54:28 +0100

    Dodawanie sal

--- Burn_Out/Burn_Out/Data/ApplicationUser.cs
+++ Burn_Out/Burn_Out/Data/ApplicationUser.cs
@@ -17,26 +17,6 @@ namespace Burn_Out.Data
         [PersonalData]
         public DateTime? DateOfBirth { get; set; }
 
-        [PersonalData]
-        [StringLength(200)]
-        public string? Address { get; set; }
-
-        [PersonalData]
-        [StringLength(100)]
-        public string? City { get; set; }
-
-        [PersonalData]
-        [StringLength(50)]
-        public string? State { get; set; }
-
-        [PersonalData]
-        [StringLength(20)]
-        public string? ZipCode { get; set; }
-
-        [PersonalData]
-        [StringLength(50)]
-        public string? Country { get; set; }
-
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         public DateTime? LastModifiedAt { get; set; }
---
+++ Burn_Out/Core/Models/Hall.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Hall
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsAvailable { get; set; }
+        public int Capacity { get; set; }
+        public DateTime? ReservationBegin { get; set; }
+        public DateTime? ReservationEnd { get; set; }
+        public int? RezervedBy { get; set; }
+    }
+}
