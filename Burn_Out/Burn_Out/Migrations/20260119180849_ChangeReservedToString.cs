ffbc1ced246fa1469a3e9ff3cc2ba4924779cfbb
Davies <167176338+Vegot795@users.noreply.github.com>
2026-01-19 19:16:55 +0100

    IT WORKS

---
+++ Burn_Out/Burn_Out/Migrations/20260119180849_ChangeReservedToString.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Burn_Out.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeReservedToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReservedBy",
+                table: "Halls",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Result",
+                table: "HallReservations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Result",
+                table: "HallReservations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReservedBy",
+                table: "Halls",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
